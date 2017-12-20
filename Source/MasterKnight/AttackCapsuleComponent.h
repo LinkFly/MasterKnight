2f9d3d6747665780512a7267a5764ed71cb90cb0
Sergey Katrevich <linkfly@yandex.ru>
2017-12-20 06:50:21 +0300

    big refactoring and other bugfix attack (enemy and player)

---
+++ Source/MasterKnight/AttackCapsuleComponent.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/CapsuleComponent.h"
+#include "AttackCapsuleComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MASTERKNIGHT_API UAttackCapsuleComponent : public UCapsuleComponent
+{
+	GENERATED_BODY()
+	
+	
+protected:
+	//NotifyActorBeginOverlap
+	
+	//virtual void OnComponentBeginOverlap() override;
+};
